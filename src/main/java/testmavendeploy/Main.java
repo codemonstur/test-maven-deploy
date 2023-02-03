d71ab9396cbd28e6ea73c8e4a61cfe576d7cbd1a
Jurgen Voorneveld <jegvoorneveld@gmail.com>
2023-02-03 22:18:21 +0100

    Updated license, messing around

--- src/main/java/testmavendeploy/Main.java
+++ src/main/java/testmavendeploy/Main.java
@@ -1,9 +1,17 @@
 package testmavendeploy;
 
+import org.fusesource.jansi.AnsiConsole;
+
+import static org.fusesource.jansi.Ansi.ansi;
+
 public class Main {
 
     public static void main(final String... args) {
-        System.out.println("Hello, world!");
+        AnsiConsole.systemInstall();
+        AnsiConsole.systemUninstall();
+        AnsiConsole.systemInstall();
+//        System.out.println(ansi().a("\u2514\u2500"));
+        System.out.println("\u2514\u2500");
     }
 
 }
